32b5af28d43667c18cafda89bc6e1f40efb07f92
d036670 <markus.strehle@sap.com>
2022-01-18 20:11:54 +0100

    add basic_auth option and pass to cf-uaa-lib

--- lib/uaa/cli/token.rb
+++ lib/uaa/cli/token.rb
@@ -25,6 +25,7 @@ class TokenCatcher < Stub::Base
     secret = server.info.delete(:client_secret)
     ti = TokenIssuer.new(Config.target, server.info.delete(:client_id), secret,
         { token_target: Config.target_value(:token_target),
+          basic_auth: Config.target_value(:basic_auth),
           skip_ssl_validation: Config.target_value(:skip_ssl_validation)})
     tkn = secret ? ti.authcode_grant(server.info.delete(:uri), data) :
         ti.implicit_grant(server.info.delete(:uri), data)
@@ -90,6 +91,7 @@ class TokenCli < CommonCli
     update_target_info
     yield TokenIssuer.new(Config.target.to_s, client_id, secret,
         { token_target: Config.target_value(:token_endpoint),
+          basic_auth: Config.target_value(:basic_auth),
           skip_ssl_validation: Config.target_value(:skip_ssl_validation),
           ssl_ca_file: Config.target_value(:ca_cert) })
   rescue Exception => e
